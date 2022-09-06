7c7d48647137df15fcc4f6c45a14cfcea59c32a5
Loki0267 <s2021045@cco.kanagawa-it.ac.jp>
2022-09-06 16:42:08 +0900

    first commit

---
+++ HelloStack/HelloStack/HelloStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloStackApp.swift
+//  HelloStack
+//
+//  Created by kiyolab02 on 2022/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
